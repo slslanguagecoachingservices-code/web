3d2b28b525fdb0598471e20e4a627a9a3a66f071
Victor A. Animasahun <ashonibarevik@gmail.com>
2025-09-03 15:32:25 +0100

    RWD update

--- scripts/script.js
+++ scripts/script.js
@@ -152,3 +152,21 @@ if (programsGrid && cards.length > 0) {
   programsGrid.addEventListener('mouseenter', stopAutoScroll);
   programsGrid.addEventListener('mouseleave', startAutoScroll);
 }
+
+/*MENU TOGGLE*/
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+const overlay = document.querySelector('.overlay');
+
+navbarToggle.addEventListener('click', () => {
+	navbarToggle.classList.toggle('active');
+	navbarMenu.classList.toggle('active');
+	overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+	// Close everything when clicking the overlay
+	navbarToggle.classList.remove('active');
+	navbarMenu.classList.remove('active');
+	overlay.classList.remove('active');
+});
